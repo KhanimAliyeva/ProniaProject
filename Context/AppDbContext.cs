cca3e04b8fd2e99861ff7ea8a9ed18606cde46f5
KhanimAliyeva <anakhanimba-mpa201@code.edu.az>
2025-12-21 16:32:23 +0400

    initial commit

---
+++ Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace Pronia.Context
+{
+    public class AppDbContext:DbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+        }
+
+        public DbSet<Slider> Sliders { get; set; }
+        public DbSet<Service> Services { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Pronia
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddDbContext<Context.AppDbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
+            });
+            var app = builder.Build();
+            app.UseStaticFiles();
+            app.MapDefaultControllerRoute();
+
+            app.Run();
+        }
+    }
+}
